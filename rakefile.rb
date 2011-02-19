45823425cad90f35a57d27743b3110da90138fea
Steven Haddox <steven@haddox.us>
2011-02-18 19:06:27 -0500

    added rake tasks for cloning & publishing

--- rakefile.rb
+++ rakefile.rb
@@ -1,12 +1,26 @@
 require 'yaml'
 
-namespace :presentor do
-  desc "Create the Sinatra project."
-  task :init do
+namespace :presentations do
+  desc "Clone the presentations from their git repositories into the application."
+  task :update do
+    system("mkdir -p presentations")
+    system("rm -rf presentations/*")
     presentations = YAML::load_file('config/presentations.yml')
     presentations.each do |presentation|
-      exec("git submodule add #{presentation[:url]} presentations/#{presentation[:title]}")
-      exec("git submodule init")
+      system("git clone #{presentation[:url]} presentations/#{presentation[:title].gsub(' ','-')}")
+    end
+  end
+  
+  desc "Create static versions of the presentations under the public directory."
+  task :publish do
+    app_root = Dir.pwd
+    system("mkdir -p public")
+    system("rm -rf public/*")
+    presentations = YAML::load_file('config/presentations.yml')
+    presentations.each do |presentation|
+      system("cd #{app_root}/presentations/#{presentation[:title].gsub(' ','-')}")
+      system("bundle exec showoff static")
+      system("mv static #{app_root}/public/#{presentation[:title]}")
     end
   end
 end
